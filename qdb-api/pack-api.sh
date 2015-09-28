a870d54d48734be5f7433d5b8649abb0b7dc8ecd
Benoit Blanchon <benoit@quasardb.net>
2015-09-28 11:54:07 +0200

    Use versioned names and symbolic links

--- qdb-api/pack-api.sh
+++ qdb-api/pack-api.sh
@@ -10,14 +10,21 @@ VERSION=$(get_version.sh $1)
 cd $(dirname $0)
 
 (
-	rm -rf 'data'
-	mkdir -p 'data/usr'
-	cd 'data/usr'
+	rm -rf data
+	mkdir -p data/usr
+	cd data/usr
 
 	tar -xf "$TARBALL"
 
-	mkdir -p 'share/qdb'
-	mv 'example' 'share/qdb/'
+    # Note: .so files are not executable on Debian
+    chmod 644 lib/libqdb_api.so 
+
+    mv lib/libqdb_api.so lib/libqdb_api-$VERSION.so   
+
+    mv include/qdb include/qdb-$VERSION
+
+	mkdir -p share/qdb/
+	mv example/ share/qdb/
 )
 
-'pack.sh' $VERSION
+pack.sh $VERSION
