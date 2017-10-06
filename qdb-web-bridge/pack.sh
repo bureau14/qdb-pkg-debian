5cfb557fea7c2eab893a0c5e65c0ae6a3914f4fd
Marek Kurdej <marek@quasardb.net>
2017-10-06 11:00:42 +0200

    Move test meta-runner script to repository.

--- qdb-web-bridge/pack.sh
+++ qdb-web-bridge/pack.sh
@@ -9,14 +9,14 @@ cd $(dirname $0)
 VERSION=$(../common/get_version.sh $TARBALL)
 
 (
-	rm -rf data
-	mkdir data
-	cd data
+    rm -rf data
+    mkdir data
+    cd data
 
-	mkdir usr
-	tar -xf "$TARBALL" -C usr
-	cp -r ../upstart/ usr/share/qdb/
-	cp -r ../systemd/ usr/share/qdb/
+    mkdir usr
+    tar -xf "$TARBALL" -C usr
+    cp -r ../upstart/ usr/share/qdb/
+    cp -r ../systemd/ usr/share/qdb/
 )
 
 ../common/pack.sh $VERSION
