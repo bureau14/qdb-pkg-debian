71193527317722c53ca463af36f9603ca8962ab8
vianney <vianney.plotton@gmail.com>
2018-08-28 16:44:26 +0200

    Properly pack.

--- qdb-api-rest/pack.sh
+++ qdb-api-rest/pack.sh
@@ -9,16 +9,17 @@ cd $(dirname $0)
 VERSION=$(../common/get_version.sh $TARBALL)
 
 (
-    rm -rf 'data'
-    mkdir -p 'data/usr'
-    cd 'data/usr'
+    rm -rf data
+    mkdir data
+    cd data
 
-    tar -xf "$TARBALL"
+    
+    mkdir usr
+    tar -xf "$TARBALL" -C usr
 
     mkdir -p usr/share/qdb/
     cp -r ../upstart/ usr/share/qdb/
     cp -r ../systemd/ usr/share/qdb/
-    cp share/qdb/default.cfg usr/share/qdb/
 )
 
 ../common/pack.sh $VERSION
