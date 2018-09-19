fc7a9278cb2337ae5e5ed6631012674a4670ef1c
Leon Mergen <leon@solatis.com>
2018-09-19 12:27:07 +0200

    Use current date as version for nightly releases

--- common/get_version.sh
+++ common/get_version.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-echo "$1" | sed -r 's/.*qdb-([0-9\.a-z_]+)-linux-.*/\1/'
+TARBALL=$1
+DATE=`which date`
+
+if [[ ${TARBALL} = *"master"* ]]
+then
+    ${DATE} +'%Y%m%d'
+else
+    echo ${TARBALL} | sed -r 's/.*qdb-([0-9\.a-z_]+)-linux-.*/\1/'
+fi
