a4127be16be120344582f79fc8d36d860fe2e57b
vianney <vianney.p@outlook.com>
2021-05-26 15:23:09 +0200

    Add test scripts.

---
+++ teamcity/test_qdb_alive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eux
+
+sleep 5
+docker-compose up -d qdb-server
+echo "qdb-server running:"
+sleep 5
+docker-compose run --rm qdb-utils '/bin/sh' '-c' "qdbsh --cluster qdb://qdb-server:2836 -c 'node_status qdb://qdb-server:2836'"
+echo "success!"
