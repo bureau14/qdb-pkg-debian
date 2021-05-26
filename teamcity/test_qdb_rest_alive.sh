5b84ff658be42e0535d2925143054208a09b633b
vianney <vianney.p@outlook.com>
2021-05-26 16:16:21 +0200

    Use qdb-rest.

--- teamcity/test_qdb_rest_alive.sh
+++ teamcity/test_qdb_rest_alive.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-docker-compose run --rm qdb-utils '/bin/bash' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
-docker-compose run --rm qdb-utils '/bin/bash' '-c' "curl -k -H 'Origin: http://0.0.0.0:3449' https://qdb-rest:40443/index.html"
+docker-compose run --rm qdb-rest '/bin/bash' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
+docker-compose run --rm qdb-rest '/bin/bash' '-c' "curl -k -H 'Origin: http://0.0.0.0:3449' https://qdb-rest:40443/index.html"
