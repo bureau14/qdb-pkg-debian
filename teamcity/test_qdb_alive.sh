1fe4e9f9b6793246eb87a5e09b917f690ed0624d
vianney <vianney.p@outlook.com>
2021-05-26 17:04:38 +0200

    Enable COMPOSE_INTERACTIVE_NO_CLI for both tests.

--- teamcity/test_qdb_alive.sh
+++ teamcity/test_qdb_alive.sh
@@ -2,9 +2,11 @@
 
 set -eux
 
+export COMPOSE_INTERACTIVE_NO_CLI=1
+
 sleep 5
 docker-compose up -d qdb-server
 echo "qdb-server running:"
 sleep 5
-docker-compose run --rm qdb-utils '/bin/bash' '-c' "qdbsh --cluster qdb://qdb-server:2836 -c 'node_status qdb://qdb-server:2836'"
+docker-compose run --rm qdb-utils '/bin/sh' '-c' "qdbsh --cluster qdb://qdb-server:2836 -c 'node_status qdb://qdb-server:2836'"
 echo "success!"
--- teamcity/test_qdb_rest_alive.sh
+++ teamcity/test_qdb_rest_alive.sh
@@ -2,11 +2,7 @@
 
 set -eux
 
-docker --version
-
 export COMPOSE_INTERACTIVE_NO_CLI=1
 
-docker-compose run --rm qdb-rest '/bin/bash' '-c' 'pwd'
-
-# docker-compose run --rm qdb-rest '/bin/bash' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
-# docker-compose run --rm qdb-rest '/bin/bash' '-c' "curl -k -H 'Origin: http://0.0.0.0:3449' https://qdb-rest:40443/index.html"
+docker-compose run --rm qdb-rest '/bin/sh' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
+docker-compose run --rm qdb-rest '/bin/sh' '-c' "curl -k -H 'Origin: http://0.0.0.0:3449' https://qdb-rest:40443/index.html"
