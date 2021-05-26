ba10335d2566d3a797477089c5169f101394cdb3
vianney <vianney.p@outlook.com>
2021-05-26 17:49:38 +0200

    Utils instead of Rest.

--- teamcity/test_qdb_rest_alive.sh
+++ teamcity/test_qdb_rest_alive.sh
@@ -4,5 +4,5 @@ set -eux
 
 export COMPOSE_INTERACTIVE_NO_CLI=1
 
-docker-compose run --rm qdb-rest '/bin/sh' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
-docker-compose run --rm qdb-rest '/bin/sh' '-c' "curl -k -H 'Origin: http://0.0.0.0:3449' https://qdb-rest:40443/index.html"
+docker-compose run --rm qdb-utils '/bin/sh' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
+docker-compose run --rm qdb-utils '/bin/sh' '-c' "curl -k -H 'Origin: http://0.0.0.0:3449' https://qdb-rest:40443/index.html"
