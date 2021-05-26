73051f2bf3cb4096b265b4b00d40c0529fc28388
vianney <vianney.p@outlook.com>
2021-05-26 16:55:29 +0200

    Check version and bypass bug.

--- teamcity/test_qdb_rest_alive.sh
+++ teamcity/test_qdb_rest_alive.sh
@@ -2,6 +2,10 @@
 
 set -eux
 
+docker --version
+
+export COMPOSE_INTERACTIVE_NO_CLI=1
+
 docker-compose run --rm qdb-rest '/bin/bash' '-c' 'pwd'
 
 # docker-compose run --rm qdb-rest '/bin/bash' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
