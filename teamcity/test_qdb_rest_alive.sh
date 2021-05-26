1d77f5117a2ed2c089de27539d65bd35855b4ce5
vianney <vianney.p@outlook.com>
2021-05-26 15:51:22 +0200

    1

--- teamcity/test_qdb_rest_alive.sh
+++ teamcity/test_qdb_rest_alive.sh
@@ -1,2 +1,6 @@
-docker-compose run --rm qdb-utils '/bin/sh' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
-docker-compose run --rm qdb-utils '/bin/sh' '-c' "curl -k -H 'Origin: http://0.0.0.0:3449' https://qdb-rest:40443/index.html"
+#!/bin/bash
+
+set -eux
+
+docker-compose run --rm qdb-utils '/bin/bash' '-c' "curl -s -k -H 'Origin: http://0.0.0.0:3449' -H 'Content-Type: application/json' -X POST https://qdb-rest:40443/api/login | grep 'credential in body is required'"
+docker-compose run --rm qdb-utils '/bin/bash' '-c' "curl -k -H 'Origin: http://0.0.0.0:3449' https://qdb-rest:40443/index.html"
