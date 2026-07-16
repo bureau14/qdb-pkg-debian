5f25ed824bbd89fb0ebc594594730da9b1c44f74
Igor <igor.niebylski@gmail.com>
2026-07-16 12:24:24 +0200

    lint

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -33,17 +33,9 @@ from qdb_pipeline import (
 STEPS_DIR = Path(__file__).parent / "steps"
 
 # Quasardb-specific toolchain overlays on top of shared infrastructure platforms.
-_LINUX = dict(
-    c_compiler="$$QDB_CICD_AGENT_CC",
-    cxx_compiler="$$QDB_CICD_AGENT_CXX",
-    asm_compiler="$$QDB_CICD_AGENT_YASM",
-    docker_image="bureau14/builder:rhel7",
-)
-_WIN = dict(
-    asm_compiler="$$QDB_CICD_AGENT_YASM",
-)
+_LINUX = dict()
 
-_OS_OVERLAY = {"linux": _LINUX, "windows": _WIN}
+_OS_OVERLAY = {"linux": _LINUX}
 PLATFORMS: list[Platform] = [
     dataclasses.replace(p, **_OS_OVERLAY.get(p.os, {}))
     for p in select_platforms("linux-amd64-haswell", "linux-aarch64")
@@ -72,7 +64,6 @@ def _env(p: Platform, step_name: str, build_type: str) -> dict[str, str]:
         OS_ENV.get(p.os, {}),
         OS_STEP_ENV.get(f"{p.os}/{step_name}", {}),
         CPU_ENV.get(p.cpu, {}),
-        {"CMAKE_BUILD_TYPE": build_type},
         platform=p,
     )
 
